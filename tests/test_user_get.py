b67f9ae86b4ffbf869d2fce4cd5f9c87084e3940
k-yudin <telesens.yudin@gmail.com>
2021-07-08 18:08:53 +0300

    added new tests that receive user's info

---
+++ tests/test_user_get.py
@@ -0,0 +1,49 @@
+import requests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+
+class TestUserGet(BaseCase):
+    def test_get_user_details_not_auth(self):
+        response = requests.get("https://playground.learnqa.ru/api/user/2")
+
+        Assertions.assert_json_has_key(response, "username")
+        Assertions.assert_json_has_not_key(response, "email")
+        Assertions.assert_json_has_not_key(response, "firstName")
+        Assertions.assert_json_has_not_key(response, "lastName")
+
+    def test_get_user_details_auth_as_same_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+
+        auth_sid = self.get_cookie(response, "auth_sid")
+        token = self.get_header(response, "x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response, "user_id")
+
+        second_response = requests.get(f"https://playground.learnqa.ru/api/user/{user_id_from_auth_method}",
+                                 headers={"x-csrf-token": token},
+                                 cookies={"auth_sid": auth_sid})
+        expected_fields = ["username", "email", "firstName", "lastName"]
+        Assertions.assert_json_has_keys(second_response, expected_fields)
+
+    def test_get_user_details_auth_as_other_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+
+        auth_sid = self.get_cookie(response, "auth_sid")
+        token = self.get_header(response, "x-csrf-token")
+        user_id = 601
+
+        second_response = requests.get(f"https://playground.learnqa.ru/api/user/{user_id}",
+                                 headers={"x-csrf-token": token},
+                                 cookies={"auth_sid": auth_sid})
+        fields = ["email", "firstName", "lastName"]
+        Assertions.assert_json_has_not_keys(second_response, fields)
+        Assertions.assert_json_has_key(second_response, "username")
