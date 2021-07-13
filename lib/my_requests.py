757237fa0e3b51aa0dd5356caa77f0b9ab192808
k-yudin <telesens.yudin@gmail.com>
2021-07-13 12:25:16 +0300

    added logger system

--- lib/my_requests.py
+++ lib/my_requests.py
@@ -1,4 +1,5 @@
 import requests
+from lib.logger import Logger
 
 class MyRequests():
     @staticmethod
@@ -26,6 +27,8 @@ class MyRequests():
         if cookies is None:
             cookies = {}
 
+        Logger.add_request(url, data, headers, cookies, method)
+
         if method == 'GET':
             response = requests.get(url, params=data, headers=headers, cookies=cookies)
         elif method == 'POST':
@@ -36,5 +39,7 @@ class MyRequests():
             response = requests.delete(url, params=data, headers=headers, cookies=cookies)
         else:
             raise Exception(f"Bad HTTP method '{method}' was received")
+
+        Logger.add_response(response)
 
         return response
